835ce1246ab9adf411d7f2662e6d06577b2e8128
ab-sid <absid24@gmail.com>
2022-11-08 12:23:51 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors())
+app.use(express.json())
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://<username>:<password>@cluster0.iikigzo.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+});
+
+
+app.get('/', (req, res) => {
+    res.send('assignment-11-swerver is running')
+})
+
+app.listen(port, () => {
+    console.log(`assignment 11 server is running on port ${port}`);
+})
